15e442e5e58cf271a7ac3aa43079801e766c25f6
BlastoiseBlue <emmet@stanevich.com>
2017-10-17 13:38:51 -0400

    Added Date class, and updated references

--- src/problem4/BlogEntry.java
+++ src/problem4/BlogEntry.java
@@ -5,9 +5,10 @@ import java.util.StringTokenizer;
 public class BlogEntry {
 	String username;
 	String postText;
-	int month;
-	int day;
-	int year;
+	// int month;
+	// int day;
+	// int year;
+	Date date = new Date();
 
 	public void setName(String inputName) {
 		username = inputName;
@@ -17,11 +18,11 @@ public class BlogEntry {
 		postText = inputText;
 	}
 
-	public void setDate(int inputMonth, int inputDay, int inputYear) {
-		month = inputMonth;
-		day = inputDay;
-		year = inputYear;
-	}
+	// public void setDate(int inputMonth, int inputDay, int inputYear) {
+	// month = inputMonth;
+	// day = inputDay;
+	// year = inputYear;
+	// }
 
 	public void clearName() {
 		setName(null);
@@ -32,7 +33,7 @@ public class BlogEntry {
 	}
 
 	public void clearDate() {
-		setDate(0, 0, 0);
+		date.setDate(0, 0, 0);
 	}
 
 	BlogEntry() {
@@ -51,12 +52,12 @@ public class BlogEntry {
 	BlogEntry(String inputName, String inputText, int inputMonth, int inputDay, int inputYear) {
 		setName(inputName);
 		setText(inputText);
-		setDate(inputMonth, inputDay, inputYear);
+		date.setDate(inputMonth, inputDay, inputYear);
 	}
 
 	public void DisplayEntry() {
-		if (month != 0 && day != 0 && year != 0)
-			System.out.print("(" + month + "/" + day + "/" + year + ") ");
+		if (date.month != 0 && date.day != 0 && date.year != 0)
+			System.out.print("(" + date.outputDate() + ") ");
 		if (username != null)
 			System.out.print(username + ": ");
 		else
@@ -68,16 +69,28 @@ public class BlogEntry {
 	}
 
 	public String getSummary() {
-		StringTokenizer outputText = new StringTokenizer(postText, " ,.");
-		// int numWords=outputText.countTokens();
+		StringTokenizer outputText = new StringTokenizer(postText, " ");
 		int x = 0;
 		String output = null;
 		for (x = 0; x < 10; x++) {
 			if (outputText.hasMoreTokens() && output != null)
-				output = output + outputText.nextToken();
+				output = output + outputText.nextToken() + " ";
 			else
-				output = outputText.nextToken();
+				output = outputText.nextToken() + " ";
 		}
 		return output;
+	}
+
+	public void PrintSummary() {
+		if (date.month != 0 && date.day != 0 && date.year != 0)
+			System.out.print("(" + date.outputDate() + ") ");
+		if (username != null)
+			System.out.print(username + ": ");
+		else
+			System.out.print("Anonymous: ");
+		if (postText != null)
+			System.out.println(getSummary());
+		else
+			System.out.println();
 	}
 }
---
+++ src/problem4/Driver.java
@@ -0,0 +1,16 @@
+/*
+ * Emmet Stanevich
+ * 10/17/17
+ * Problem 4
+ */
+package problem4;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		BlogEntry blog1=new BlogEntry("Emmet Stanevich","Hello there, I am happy to see you! I hope you have a good day",10,17,2017);
+		blog1.DisplayEntry();
+		blog1.PrintSummary();
+	}
+
+}
