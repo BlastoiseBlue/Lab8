f1883fdd4fe8a1487e869d251d610c9ec8693969
BlastoiseBlue <emmet@stanevich.com>
2017-10-17 12:48:33 -0400

    Renamed post() to DisplayEntry()

--- src/problem4/BlogEntry.java
+++ src/problem4/BlogEntry.java
@@ -6,44 +6,62 @@ public class BlogEntry {
 	int month;
 	int day;
 	int year;
+
 	public void setName(String inputName) {
-		username=inputName;
+		username = inputName;
 	}
+
 	public void setText(String inputText) {
-		postText=inputText;
+		postText = inputText;
 	}
+
 	public void setDate(int inputMonth, int inputDay, int inputYear) {
-		month=inputMonth;
-		day=inputDay;
-		year=inputYear;
+		month = inputMonth;
+		day = inputDay;
+		year = inputYear;
 	}
+
 	public void clearName() {
 		setName(null);
 	}
+
 	public void clearText() {
 		setText(null);
 	}
+
 	public void clearDate() {
-		setDate(0,0,0);
+		setDate(0, 0, 0);
 	}
-	BlogEntry(){
-		
+
+	BlogEntry() {
+
 	}
-	BlogEntry(String inputName){
+
+	BlogEntry(String inputName) {
 		setName(inputName);
 	}
-	BlogEntry(String inputName, String inputText){
+
+	BlogEntry(String inputName, String inputText) {
 		setName(inputName);
 		setText(inputText);
 	}
-	BlogEntry(String inputName, String inputText, int inputMonth, int inputDay, int inputYear){
+
+	BlogEntry(String inputName, String inputText, int inputMonth, int inputDay, int inputYear) {
 		setName(inputName);
 		setText(inputText);
 		setDate(inputMonth, inputDay, inputYear);
 	}
-	public void post() {
-		if(month!=0&&day!=0&&year!=0)System.out.print("("+month+"/"+day+"/"+year+") ");
-		if(username!=null)System.out.print(username+": ");else System.out.print("Anonymous: ");
-		if(postText!=null)System.out.println(postText);else System.out.println();
+
+	public void DisplayEntry() {
+		if (month != 0 && day != 0 && year != 0)
+			System.out.print("(" + month + "/" + day + "/" + year + ") ");
+		if (username != null)
+			System.out.print(username + ": ");
+		else
+			System.out.print("Anonymous: ");
+		if (postText != null)
+			System.out.println(postText);
+		else
+			System.out.println();
 	}
 }
