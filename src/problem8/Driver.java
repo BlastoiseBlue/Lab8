da9243eb2e9d911c53cf1fde3fe4a1482a814740
BlastoiseBlue <emmet@stanevich.com>
2017-10-24 12:01:04 -0400

    Wrote driver program

--- src/problem8/Driver.java
+++ src/problem8/Driver.java
@@ -8,7 +8,21 @@ package problem8;
 public class Driver {
 
 	public static void main(String[] args) {
-		
+		Date date1 = new Date(6, 13, 1999);
+		Date date2 = new Date();
+		Date date3 = new Date("OcToBeR", 31, 2017);
+		System.out.println("Date 1:");
+		System.out.println(date1.outputDate());
+		System.out.println(date1.outputDateString());
+		System.out.println(date1.convertMonth(date1.getMonth()));
+		System.out.println("Date 2:");
+		System.out.println(date2.outputDate());
+		System.out.println(date2.outputDateString());
+		System.out.println(date2.getYear());
+		System.out.println("Date 3:");
+		System.out.println(date3.outputDate());
+		System.out.println(date3.outputDateString());
+		System.out.println(date3.getDay());
 	}
 
 }
